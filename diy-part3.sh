2c539df358f01a7042dee345113f82765f651b35
wuzibin <38431651+wuzibin@users.noreply.github.com>
2020-11-22 13:59:56 +0800

    Change git path

--- diy-part3.sh
+++ diy-part3.sh
@@ -14,22 +14,22 @@
 # sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
 #Add luci-app-dogcom
-git clone https://github.com/mchome/openwrt-dogcom package/feeds/packages/dogcom
-git clone https://github.com/mchome/luci-app-dogcom package/feeds/luci/luci-app-dogcom
+git clone https://github.com/mchome/openwrt-dogcom package/lean/dogcom
+git clone https://github.com/mchome/luci-app-dogcom package/lean/luci-app-dogcom
 
 #Add luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb package/lean/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr package/lean/luci-app-vssr
 
 #Add luci-app-scutclient
-git clone https://github.com/wuzibin/scutclient package/feeds/packages/scutclient
-git clone https://github.com/wuzibin/luci-app-scutclient package/feeds/luci/luci-app-scutclient
+git clone https://github.com/wuzibin/scutclient package/lean/scutclient
+git clone https://github.com/wuzibin/luci-app-scutclient package/lean/luci-app-scutclient
 
 #Add luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/feeds/luci/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
 
 #Add luci-app-jd-dailybonus
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/feeds/luci/luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
 
 #Add luci-app-serverchan
-git clone https://github.com/tty228/luci-app-serverchan.git package/feeds/luci/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
